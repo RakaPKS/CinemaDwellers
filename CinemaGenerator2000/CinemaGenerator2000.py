b590bfe6d9ab3e15eb049864f1d8a8c93291f1ca
Gideon Ogilvie <g.a.ogilvie@students.uu.nl>
2020-09-10 14:48:45 +0200

    added online instance generator

--- CinemaGenerator2000/CinemaGenerator2000.py
+++ CinemaGenerator2000/CinemaGenerator2000.py
@@ -10,6 +10,7 @@ n_columns_break = 5
 n_rows_break = 5
 perc_empty = 0.2
 perc_seats_occupied = 0.3
+online = True
 
 if not debug:
     n_rows = int(input("Number of rows: "))
@@ -18,6 +19,7 @@ if not debug:
     n_columns_break = int(input("Every n seats add an empty column: "))
     perc_empty = float(input("Percentage of empty spaces: "))
     perc_seats_occupied = float(input("Percentage of seats occupied: "))
+    online = bool(input("Online(boolean):" ))
 
 def generate():
     cinema = np.full((n_rows, n_columns), 1)
@@ -50,7 +52,7 @@ def pretty_print(cinema):
 
 def write_to_file(cinema, group_sizes):
     pretty_cinema = pretty_print(cinema)
-    f = open("cinema.txt", "w")
+    f = open("cinema_online.txt" if online else "cinema_offline.txt", "w")
     f.write(str(n_rows) + "\n")
     f.write(str(n_columns) + "\n")
     f.write(pretty_cinema)
@@ -67,7 +69,7 @@ def count_seats(cinema):
                 n_of_people += 1
     return n_of_people
 
-def generate_groups(cinema):
+def generate_groups_offline(cinema):
     ## first calculate total number of seats available
     ## do we always generate group sizes up to the max seats available
     group_sizes = np.full(8, 0)
@@ -104,8 +106,45 @@ def generate_groups(cinema):
 
     return group_sizes
 
+def generate_groups_online(cinema):
+    group_sizes = np.full(0, int)
+    n_of_people = 0;
+    n_of_seats = perc_seats_occupied * count_seats(cinema)
+
+
+    while(n_of_people < n_of_seats):
+        rand = random.random()
+
+        if rand < 0.2:
+            group_sizes = np.append(group_sizes, 1)
+            n_of_people += 1
+        elif rand < 0.4:
+            group_sizes = np.append(group_sizes, 2)
+            n_of_people += 2
+        elif rand < 0.6:
+            group_sizes = np.append(group_sizes, 3)
+            n_of_people += 3
+        elif rand < 0.7:
+            group_sizes = np.append(group_sizes, 4)
+            n_of_people += 4
+        elif rand < 0.8:
+            group_sizes = np.append(group_sizes, 5)
+            n_of_people += 5
+        elif rand < 0.9:
+            group_sizes = np.append(group_sizes, 6)
+            n_of_people += 6
+        elif rand < 0.95:
+            group_sizes = np.append(group_sizes, 7)
+            n_of_people += 7
+        else:
+            group_sizes = np.append(group_sizes, 8)
+            n_of_people += 8
+
+    group_sizes = np.append(group_sizes, 0)
+    return group_sizes
+
 cinema = generate()
-groups = generate_groups(cinema)
+groups = generate_groups_online(cinema) if online else generate_groups_offline(cinema)
 write_to_file(cinema, groups)
 
 print("Done")
